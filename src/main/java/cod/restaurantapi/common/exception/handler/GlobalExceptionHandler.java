54f985737c1c5a042f4a304f05436c543b41592d
Abdullah Çelik <celika80@hotmail.com>
2024-04-16 00:53:43 +0300

    cantReadable exception is removed

--- src/main/java/cod/restaurantapi/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/cod/restaurantapi/common/exception/handler/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -93,18 +92,6 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<RMAError> handleMesssageNotReadableException(final HttpMessageNotReadableException exception) {
-
-        log.error(exception.getMessage(), exception);
-
-        RMAError error = RMAError.builder()
-                .message(exception.getMessage())
-                .status(HttpStatus.BAD_REQUEST)
-                .build();
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
