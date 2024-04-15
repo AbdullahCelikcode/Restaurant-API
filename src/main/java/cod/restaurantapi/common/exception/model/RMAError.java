ffaf5d00b2a439f7ac535f40750cf9f2733f0e04
Abdullah Çelik <celika80@hotmail.com>
2024-04-15 16:52:05 +0300

    error class configured

--- src/main/java/cod/restaurantapi/common/exception/model/RMAError.java
+++ src/main/java/cod/restaurantapi/common/exception/model/RMAError.java
@@ -1,19 +1,36 @@
 package cod.restaurantapi.common.exception.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.http.HttpStatusCode;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Builder
 public class RMAError {
+
     @Builder.Default
     private LocalDateTime timestamp = LocalDateTime.now();
 
     private HttpStatusCode status;
 
-    private String massage;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private List<SubErrors> errors;
+
+    @Getter
+    @Builder
+    public static class SubErrors {
+
+        private String field;
+
+        private String message;
+    }
+
 
 }
