d1ce277f0536a73b181a2cd7398654a0659ce040
Abdullah Çelik <celika80@hotmail.com>
2024-04-16 03:46:08 +0300

    CategoryAddRequest all and no args constructor added because this class wasnt readable for test

--- src/main/java/cod/restaurantapi/category/controller/request/CategoryAddRequest.java
+++ src/main/java/cod/restaurantapi/category/controller/request/CategoryAddRequest.java
@@ -1,12 +1,18 @@
-package cod.restaurantapi.product.controller.request;
+package cod.restaurantapi.category.controller.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryAddRequest {
     @NotBlank
     @Size(min = 3, max = 255)
