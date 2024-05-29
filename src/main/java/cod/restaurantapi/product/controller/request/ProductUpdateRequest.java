2fc3a110fc2ed3b9c1f3b5251ef70d77b59534e6
Abdullah Çelik <celika80@hotmail.com>
2024-05-29 18:13:50 +0300

    added Assertions for enums

--- src/main/java/cod/restaurantapi/product/controller/request/ProductUpdateRequest.java
+++ src/main/java/cod/restaurantapi/product/controller/request/ProductUpdateRequest.java
@@ -2,6 +2,8 @@ package cod.restaurantapi.product.controller.request;
 
 import cod.restaurantapi.product.model.enums.ExtentType;
 import cod.restaurantapi.product.model.enums.ProductStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -10,6 +12,7 @@ import lombok.Getter;
 import org.hibernate.validator.constraints.Range;
 
 import java.math.BigDecimal;
+import java.util.EnumSet;
 
 @Getter
 @Builder
@@ -41,5 +44,39 @@ public class ProductUpdateRequest {
     @NotNull
     @Range(min = 0, max = Integer.MAX_VALUE)
     private Long categoryId;
+
+    @JsonIgnore
+    @AssertTrue
+    @SuppressWarnings("This method is unused by the application directly but Spring is using it in the background.")
+    private boolean isProductStatusValid() {
+
+        if (this.status == null) {
+            return true;
+        }
+
+        EnumSet<ProductStatus> acceptableStatus = EnumSet.of(
+                ProductStatus.ACTIVE,
+                ProductStatus.INACTIVE,
+                ProductStatus.DELETED
+        );
+        return acceptableStatus.contains(this.status);
+    }
+
+    @JsonIgnore
+    @AssertTrue
+    @SuppressWarnings("This method is unused by the application directly but Spring is using it in the background.")
+    private boolean isExtentTypeValid() {
+
+        if (this.extentType == null) {
+            return true;
+        }
+
+        EnumSet<ExtentType> acceptableStatus = EnumSet.of(
+                ExtentType.ML,
+                ExtentType.GR
+        );
+        return acceptableStatus.contains(this.extentType);
+    }
+
 
 }
