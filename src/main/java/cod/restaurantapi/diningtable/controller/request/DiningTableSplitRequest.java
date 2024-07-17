1bbb2442dc9d25e31970ad9e1135578ab99aeaf4
Abdullah Çelik <76781045+AbdullahCelikcode@users.noreply.github.com>
2024-07-17 16:23:02 +0300

    DiningTableSplitRequest created

---
+++ src/main/java/cod/restaurantapi/diningtable/controller/request/DiningTableSplitRequest.java
@@ -0,0 +1,21 @@
+package cod.restaurantapi.diningtable.controller.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DiningTableSplitRequest {
+
+    @NotNull
+    private UUID mergeId;
+}
