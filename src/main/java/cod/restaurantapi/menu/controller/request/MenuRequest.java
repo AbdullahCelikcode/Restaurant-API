32eda14b19f4dc3408d9157618e2b5fd083de1b2
Abdullah Çelik <76781045+AbdullahCelikcode@users.noreply.github.com>
2024-07-17 16:25:48 +0300

    isPropertyAccepted method added to all ListRequest

--- src/main/java/cod/restaurantapi/menu/controller/request/MenuRequest.java
+++ src/main/java/cod/restaurantapi/menu/controller/request/MenuRequest.java
@@ -2,12 +2,16 @@ package cod.restaurantapi.menu.controller.request;
 
 import cod.restaurantapi.common.controller.request.RMAListRequest;
 import cod.restaurantapi.common.model.RMAFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.AssertTrue;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+
+import java.util.Set;
 
 @Getter
 @SuperBuilder
@@ -27,4 +31,14 @@ public class MenuRequest extends RMAListRequest {
         private String name;
 
     }
+
+    @JsonIgnore
+    @AssertTrue
+    @Override
+    public boolean isOrderPropertyAccepted() {
+        final Set<String> acceptedFilterFields = Set.of();
+        return this.isPropertyAccepted(acceptedFilterFields);
+    }
+
+
 }
